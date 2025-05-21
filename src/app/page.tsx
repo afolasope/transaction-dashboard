9431798cc802633d69ba9c56942bd68bfbbead18
afolasope <folasopeadebanjo@gmail.com>
2025-05-21 10:03:47 +0100

    update page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,68 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+import ActivityChart from "@/components/ActivityChart";
+import TransactionCard from "@/components/TransactionCard";
+import TransactionStats from "@/components/TransactionStats";
+import { Skeleton } from "@/components/ui/skeleton";
+import { fetchTransactions } from "@/lib/api";
+import { activityData } from "@/lib/mockData";
+import { useTransactionStatsStore } from "@/store/useTransactionStatsStore";
+import { useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+export default function Dashboard() {
+    const setStats = useTransactionStatsStore((state) => state.setStats);
+    const { data, isLoading, isError } = useQuery({
+        queryKey: ["transactions"],
+        queryFn: fetchTransactions,
+    });
+
+    useEffect(() => {
+        if (data) {
+            const completed = data.filter((tx) => tx.status === "completed").length;
+            const disputed = data.filter((tx) => tx.isDisputed === true).length;
+            const inProgress = data.filter((tx) => tx.status === "in_progress").length;
+
+            setStats({ completed, disputed, inProgress });
+        }
+    }, [data]);
+
+    return (
+        <div className='px-4'>
+            {isLoading ? (
+                <div>
+                    <div className='grid gap-4 p-4'>
+                        <Skeleton className='h-56 w-full' />
+                        <Skeleton className='h-56 w-full' />
+                        <Skeleton className='h-56 w-full' />
+                    </div>
+                </div>
+            ) : isError || !data ? (
+                <p className='text-red-600 p-4'>Something went wrong.</p>
+            ) : (
+                <div className='flex flex-col gap-4 py-8'>
+                    <h1 className='text-3xl'>Active Transactions</h1>
+                    <div className='px-4 w-full flex-1'>
+                        <TransactionStats />
+                    </div>
+                    {data.length > 0 ? (
+                        <div className='flex flex-col gap-4 lg:flex-row w-full'>
+                            <div className='grid gap-4 flex-1'>
+                                {data.map((transaction) => (
+                                    <TransactionCard key={transaction.id} transaction={transaction} />
+                                ))}
+                            </div>
+                            {/* <div className=''> */}
+                            <ActivityChart data={activityData} />
+                            {/* </div> */}
+                        </div>
+                    ) : (
+                        <div className=''>
+                            <h2 className='text-2xl font-semibold mb-4'>No transactions yet.</h2>
+                        </div>
+                    )}
+                </div>
+            )}
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
 }
